08452ebd97da82d24533e84bfec857ba17cb1862
Gianbdf <gian.defaria@hotmail.com>
2023-03-06 20:13:25 -0300

    importando icone home

--- src/components/side-bar/side-bar.tsx
+++ src/components/side-bar/side-bar.tsx
@@ -1,5 +1,7 @@
 import "./side-bar.css"
 import logo from "../../assets/logo.svg"
+import home from "../../assets/icon-nav-home.svg"
+import movie from "../../assets/icon-nav-movies.svg"
 
 
 export default function SideBar () {
@@ -9,6 +11,7 @@ export default function SideBar () {
             <section className="side-bar-icons-container">
                 <img src={logo} alt="logo" className="side-bar-logo"/>
                 <div className="side-bar-icons">
+                    <img src={home} alt="home-icon" />
 
                 </div>
             </section>
