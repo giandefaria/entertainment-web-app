1baf3b594a4974bb179681836c15060b3e2ad253
Gianbdf <gian.defaria@hotmail.com>
2023-03-07 20:18:01 -0300

    adicionando sideBar no app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './global.css'
+import SideBar from "./components/side-bar/side-bar"
 import Main from './pages/main/main';
 import { Routes, Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom'
@@ -9,6 +10,7 @@ import { BrowserRouter } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+      <SideBar />
       <BrowserRouter> 
         <Routes>
           <Route path="/" element ={ <Main />} />
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,11 +1,9 @@
-import SideBar from "../../components/side-bar/side-bar"
 
 export default function Main () {
 
 
     return (
         <>
-            <SideBar />
             <div>
                 <h1>página principal</h1>
             </div>
