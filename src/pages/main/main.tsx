384d360935f731bc0c961aa9b31effb4bb61cc0e
Gianbdf <gian.defaria@hotmail.com>
2023-03-07 20:26:33 -0300

    adicionando input de busca

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -3,11 +3,14 @@ export default function Main () {
 
 
     return (
-        <>
+        <main>
+            <div className="input-container">
+                <input type="text" placeholder="Search for movies or TV series" />
+            </div>
             <div>
                 <h1>página principal</h1>
             </div>
-        </>
+        </main>
 
     )
 
