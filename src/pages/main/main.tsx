7d4445f6b58d70684b89fd5e94b44b925d5c25f8
Gianbdf <gian.defaria@hotmail.com>
2023-03-07 20:34:55 -0300

    tirando a borda e a cor de fundo do input de procura

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -6,7 +6,7 @@ export default function Main () {
     return (
         <main>
             <div className="input-container">
-                <img src={icon} alt="icon-search" /><input type="text" placeholder="Search for movies or TV series" />
+                <img src={icon} alt="icon-search" /><input type="text" placeholder="Search for movies or TV series" className="search-input"/>
             </div>
             <div>
                 <h1>página principal</h1>
