6c6016ea8b4673439a50db2a63d1ef162e10b440
Gianbdf <gian.defaria@hotmail.com>
2023-03-08 20:59:17 -0300

    converto a resposta em um arquivo json

--- src/functions/api-connect.ts
+++ src/functions/api-connect.ts
@@ -1,6 +1,8 @@
 export async function conectar() {
     const resposta = await fetch('http://localhost:3333/movies');
     console.log(resposta);
+    const respostaConvertida = await resposta.json();
+    console.log(respostaConvertida);
 }
 
 console.log('ok')
