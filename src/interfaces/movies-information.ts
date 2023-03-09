b8ed6952b087d5059b5e5b6ee2484b6ba7555c0d
Gianbdf <gian.defaria@hotmail.com>
2023-03-09 20:08:07 -0300

    criando interface do objeto

--- src/interfaces/movies-information.ts
+++ src/interfaces/movies-information.ts
@@ -1,4 +1,11 @@
 export interface moviesInformation {
 
-    
+    category : string,
+    isBookmarked : boolean,
+    isTrending : boolean,
+    rating : string,
+    thumbnail : any,
+    title : string,
+    year : number
+
 }
