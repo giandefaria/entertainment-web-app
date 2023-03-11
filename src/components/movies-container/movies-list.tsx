cfaafb82856f21e16fea94aec65a43b184bd831f
Gianbdf <gian.defaria@hotmail.com>
2023-03-11 18:14:26 -0300

    adicionando função de adicionar na lista

--- src/components/movies-container/movies-list.tsx
+++ src/components/movies-container/movies-list.tsx
@@ -4,22 +4,23 @@ import thumb from "../../assets/thumbnails/the-great-lands/regular/small.jpg"
 import bookempy from "../../assets/icon-bookmark-empty.svg"
 import iconMovie from "../../assets/icon-category-movie.svg"
 
+
 export default function MovieList () {
 
-    return (
-        <li className="movies-container">
-            <div className="thumb-container">
-                <img src={thumb} alt="thumb" /> <div className="bookmarked-icon"><img src={bookempy} alt="bookmarked-icon"/></div>
+    return (`
+        <li class="movies-container">
+            <div class="thumb-container">
+                <img src='${thumb}' alt="thumb" /> <div class="bookmarked-icon"><img src='${bookempy}' alt="bookmarked-icon"/></div>
             </div>
 
-            <div className="category">
-            <h6>2019</h6> <div className="list-style display-center"><img src={iconMovie} alt="icon-movie" /> <h6> Movie</h6></div> <h6 className="list-style">PG</h6>
+            <div class="category">
+            <h6>2019</h6> <div class="list-style display-center"><img src='${iconMovie}' alt="icon-movie" /> <h6> Movie</h6></div> <h6 class="list-style">PG</h6>
             </div>
             <h4>The Great Lands</h4> 
         </li>
 
 
-    )
+    `)
 
 
 }
--- src/functions/add-movies.ts
+++ src/functions/add-movies.ts
@@ -1,5 +1,6 @@
 //função que adicionará os filmes nos comentários
 
+import MovieList from "../components/movies-container/movies-list";
 import { conectar } from "./api-connect"; //função que conecta e retorna uma lista da api
 
 export default async function addMovies (elements: NodeListOf<HTMLElement>) {
@@ -8,6 +9,32 @@ export default async function addMovies (elements: NodeListOf<HTMLElement>) {
 
     elements.forEach( (htmlElement: HTMLElement) =>  {
         console.log(htmlElement.dataset.ul);
+
+        if (htmlElement.dataset.ul == 'notTreding') {
+            htmlElement.innerHTML= ""
+            notTrendingMovie(obj, htmlElement);
+            
+                        
+        }
+
     } )
 
+}
+
+function notTrendingMovie(obj: any, htmlElement: HTMLElement) {
+    const objFiltrado = obj.filter((movie: any) => {
+        return movie.isTrending == false;
+        
+    });
+
+    objFiltrado.forEach((element: any) => {
+        htmlElement.innerHTML = htmlElement.innerHTML + MovieList();
+    })
+
+    console.log(objFiltrado);
+    //console.log(htmlElement)
+    //const lista = return MovieList();
+    //console.log(JSON.stringify(lista))
+    //htmlElement.appendChild(lista)
+    
 }
