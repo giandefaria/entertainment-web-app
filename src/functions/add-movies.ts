d145a34e4d2c2f3d6aae4463fe8aa2f3fdd74d68
Gianbdf <gian.defaria@hotmail.com>
2023-03-15 21:13:35 -0300

    organizando a verificação em novas funções

--- src/functions/add-movies.ts
+++ src/functions/add-movies.ts
@@ -4,42 +4,48 @@ import movieList from "../components/movies-container/movies-list";
 import { moviesInformation } from "../interfaces/movies-information";
 import { conectar } from "./api-connect"; //função que conecta e retorna uma lista da api
 
-export default async function addMovies (elements: NodeListOf<HTMLElement>) {
+export default async function addMovies (NodeElements: NodeListOf<HTMLElement>) {
     const obj = await conectar(); //atribuo o retorno ao obj
     console.log(obj);
 
-    elements.forEach( (htmlElement: HTMLElement) =>  {
+    NodeElements.forEach( (htmlElement: HTMLElement) =>  {
         verifyCategoryAndAddListOfMovies(obj, htmlElement);
 
     })
 
 }
 
-//função que filtra os itens do objeto em que o trending seja falso
+//função que verificará a nodeList e adicionará o respectivo filme
 function verifyCategoryAndAddListOfMovies(obj: any, htmlElement: HTMLElement) {
     console.log(htmlElement.dataset.ul); //retorna se é trending ou not trending
-    
-
-    if (htmlElement.dataset.ul == 'trending') { //filmes que estão entre os trending
-        const objFiltrado = obj.filter((movie: any) => {
-            return movie.isTrending == true;            
-           
-        });
-        console.log(objFiltrado) 
-        addMovieListInHtml(objFiltrado, htmlElement);
-    }
-
-    if (htmlElement.dataset.ul == 'notTreding') { //filmes que não estao entre os trending
-        const objFiltrado = obj.filter((movie: any) => {
-            return movie.isTrending == false;            
-           
-        });
-        console.log(objFiltrado) 
-        addMovieListInHtml(objFiltrado, htmlElement);
-    }
+ 
+    tredingMoviesList(htmlElement, obj);
+    notTredingMoviesList(htmlElement, obj);
     
     //console.log(objFiltrado);
     
+}
+
+function notTredingMoviesList(htmlElement: HTMLElement, obj: any) {
+    if (htmlElement.dataset.ul == 'notTreding') { //filmes que não estao entre os trending
+        const objFiltrado = obj.filter((movie: any) => {
+            return movie.isTrending == false;
+
+        });
+        console.log(objFiltrado);
+        addMovieListInHtml(objFiltrado, htmlElement);
+    }
+}
+
+function tredingMoviesList(htmlElement: HTMLElement, obj: any) {
+    if (htmlElement.dataset.ul == 'trending') { //filmes que estão entre os trending
+        const objFiltrado = obj.filter((movie: any) => {
+            return movie.isTrending == true;
+
+        });
+        console.log(objFiltrado);
+        addMovieListInHtml(objFiltrado, htmlElement);
+    }
 }
 
 //função que adiciona a lista no html da página
