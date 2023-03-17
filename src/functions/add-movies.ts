e6e1701517440dffb9fc8ea155255baae362fb5c
Gianbdf <gian.defaria@hotmail.com>
2023-03-17 18:29:52 -0300

    capturando valor do título

--- src/functions/add-movies.ts
+++ src/functions/add-movies.ts
@@ -49,6 +49,7 @@ function addMovieListInHtml(objFiltrado: any, htmlElement: HTMLElement) {
     
     objFiltrado.forEach((obj: moviesInformation) => {
         const thumbnail = (obj.thumbnail.regular.small); //capturo link da thumb para enviar a função movieList
+        const title =(obj.title);
         htmlElement.innerHTML = htmlElement.innerHTML + movieList(thumbnail);
     });
 }
