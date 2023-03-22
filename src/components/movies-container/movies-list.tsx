c2d26cdca8273d90f7c4e8ae5c133f4862ede6fa
Gianbdf <gian.defaria@hotmail.com>
2023-03-22 20:42:24 -0300

    adicionando outra div no html do trending

--- src/components/movies-container/movies-list.tsx
+++ src/components/movies-container/movies-list.tsx
@@ -15,10 +15,12 @@ export function trendingList (slideNumber:number,thumbnail:any, year:number, cat
                 <img class="thumb-image-trending" src='${thumbnail}' alt="thumb" /> <div class="bookmarked-icon"><img src='${bookempy}' alt="bookmarked-icon"/></div>
             </div>
 
-            <div class="category">
-            <h6>${year}</h6> <div class="list-style display-center"><img src='${iconMovie}' alt="icon-movie" /> <h6> ${category}</h6></div> <h6 class="list-style">${rating}</h6>
-            </div>
-            <h4 class="movie-title">${title}</h4> 
+            <div>
+                <div class="category">
+                <h6>${year}</h6> <div class="list-style display-center"><img src='${iconMovie}' alt="icon-movie" /> <h6> ${category}</h6></div> <h6 class="list-style">${rating}</h6>
+                </div>
+                <h4 class="movie-title">${title}</h4>
+            </div> 
         </li>
 
 
